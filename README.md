2008d0ac9757466b30a2362c74edd48ea6f8a6c1
Kevin1353 <81064392+Kevin1353@users.noreply.github.com>
2021-06-14 13:53:51 -0700

    Primary program is complete
    This is now a fully functioning program that requests user input for a folder and then deletes all files within it that are older than a certain date

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # file-cleaner
 Simple program used to clean old files on a Windows PC.
 
-This program automatically deletes files in a specified folder that are older than a specified date.
+This program automatically deletes files in a specified folder that are older than a specified age.
 
 Created by Kevin Anderson 6-10-2021
--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 # Import required modules
 import os
+import time
 
-# The following path determines the folder we will be cleaning up
-path = "C:/Users/kev12/Downloads/02_Classwork_2020_Networks1.docx"
+
+# main function to call other functions
+def main():
+    path = get_path()
+    age = get_age()
+    file_list = get_file(path)
+    for file in file_list:
+        full_file = path + file  # this combines the file path with the specific file name
+        file_age = is_file_old(full_file, age)
+        if file_age == 1:
+            delete_file(full_file)
+
+
+# asks user to input a path for the folder they would like cleaned
+def get_path():
+    return input("Please enter the full file path to be cleaned (ex: C:/Users/kev12/Downloads/): ")
+
+
+# asks user to input an age where they would like files to be deleted
+def get_age():
+    return int(input("How many days old must files be to be deleted: "))
 
 
 # this gets the individual file that we will be using
-def get_file():
-    os.listdir(path)  # this lists all files in a directory
+def get_file(path):
+    return os.listdir(path)  # this lists all files in a directory
 
 
-# this finds the time of a file
-age = os.stat(path)
+# this function determines if a file is old enough that it should be deleted
+def is_file_old(file, age):
+    file_age = os.stat(file)
+    seconds = time.time() - file_age.st_mtime  # this is the current seconds since Epoch minus the file's current
+    # seconds since Epoch
+    if age <= (((seconds/60)/60)/24):  # convert the seconds to days and compare to the expected age
+        return 1
 
-# this removes a specific file
-# os.remove("C:/Users/kev12/Downloads/MBSetup.exe")
 
-print(age.st_mtime)
+# this function deletes the given file
+def delete_file(file):
+    os.remove(file)
+
+
+# call main
+main()
